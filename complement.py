95361ce74727045a4171798659ac0ddf984e2c56
Yoshiaki Araki <yoshiaki.araki@g-pc-4601.intra.gree-office.net>
2017-03-10 18:15:05 +0900

    complement

---
+++ complement.py
@@ -0,0 +1,150 @@
+# coding: utf-8
+import dlx as dlx
+
+def makeTiling(magic,combo):
+
+    tbl = []
+    for x in xrange(0, magic):
+        size = 0
+        for cv in combo:
+            size+=1
+            if ( (cv>0) and ( x <= magic - size) ) :
+                tmp = []
+                for y in xrange(0, size):
+                    tmp.append(x+y)
+                tbl.append(tmp)
+    #print tbl
+    return tbl
+
+def solve(combo):
+    check = []
+    chcount=0
+    for cv in combo:
+        chcount+=1
+        for ci in xrange(0,cv):
+            check.append(str(chcount))
+    checkstr = ",".join(check)
+    magic = 12
+    csum = 0
+    for cv in combo:
+        csum += cv 
+
+    raw = []
+    compiled = []
+
+    c = [0]
+    def counter(xs):
+      
+        if (True):
+            ele2 = []
+            for xa in xs:
+                for xv in xa:
+                    ele2.append(xv)
+
+            if len(ele2)!=magic:
+                return
+            if (len(list(set(ele2)))!=magic):
+                return
+        
+        if (len(xs)==csum):
+          #print xs
+          msum = 0
+          for xv in xs:
+              msum += len(xv)
+             
+          if msum == magic:
+            output = []
+            ele = []
+            for xv in xs:
+                output.append(str(len(xv)))
+                ele.append(str(len(xv)))
+            ele.sort()
+            #print output
+            if ",".join(ele) == checkstr:
+                #print output
+                raw.append(",".join(output))
+                for xv in xs:
+                    output.append(str(len(xv)))
+                output.append(":")
+                rxs = list(xs)
+                rxs.reverse()
+                for rxv in rxs:
+                    output.append(str(len(rxv)))
+                for rxv in rxs:
+                    output.append(str(len(rxv)))
+                j = ",".join(output)
+                compiled.append(j)
+                c[0] += 1
+   
+    ls = makeTiling(magic,combo)
+    dlx.algoDLX(counter, ls)
+
+    same = []
+    for cv in compiled:
+        same.append(-1)
+    rcount = 0
+    for rv in raw:
+        ccount =0
+        for cv in compiled:
+            if same[ccount]==-1:
+                if cv.find(rv)>=0:
+                    same[ccount]=rcount
+            ccount+=1
+        rcount+=1
+    #print c[0]
+    uniq = list(set(same))
+    result = []
+    for ui in uniq:
+        result.append(raw[ui])
+    print len(result)
+    #print result
+    return c[0]
+
+if(True):
+    solve([10,1,0,0,0])
+    solve([8,2,0,0,0])
+    solve([6,3,0,0,0])
+    solve([4,4,0,0,0])
+    solve([2,5,0,0,0])
+    solve([0,6,0,0,0])
+    solve([9,0,1,0,0])
+    solve([7,1,1,0,0])
+    solve([5,2,1,0,0])
+    solve([3,3,1,0,0])
+    solve([1,4,1,0,0])
+    solve([6,0,2,0,0])
+    solve([4,1,2,0,0])
+    solve([2,2,2,0,0])
+    solve([0,3,2,0,0])
+    solve([3,0,3,0,0])
+    solve([1,1,3,0,0])
+    solve([0,0,4,0,0])
+    solve([8,0,0,1,0])
+    solve([6,1,0,1,0])
+    solve([4,2,0,1,0])
+    solve([2,3,0,1,0])
+    solve([0,4,0,1,0])
+    solve([5,0,1,1,0])
+    solve([3,1,1,1,0])
+    solve([1,2,1,1,0])
+    solve([2,0,2,1,0])
+    solve([0,1,2,1,0])
+    solve([4,0,0,2,0])
+    solve([2,1,0,2,0])
+    solve([0,2,0,2,0])
+    solve([1,0,1,2,0])
+    solve([0,0,0,3,0])
+    solve([7,0,0,0,1])
+    solve([5,1,0,0,1])
+    solve([3,2,0,0,1])
+    solve([1,3,0,0,1])
+    solve([4,0,1,0,1])
+    solve([2,1,1,0,1])
+    solve([0,2,1,0,1])
+    solve([1,0,2,0,1])
+    solve([3,0,0,1,1])
+    solve([1,1,0,1,1])
+    solve([0,0,1,1,1])
+    solve([2,0,0,0,2])
+    solve([0,1,0,0,2])
+    solve([12,0,0,0,0])
---
+++ dlx.py
@@ -0,0 +1,125 @@
+
+
+# coding: utf-8
+import time
+
+Header = None
+
+class Dnode:
+    def __init__(self, n):
+        self.up   = self
+        self.down = self
+        self.next = self
+        self.prev = self
+        self.num  = n      
+        self.head = self  
+        self.size = 0     
+
+def initHeader():
+    global Header
+    Header = Dnode(-1)
+
+def insertLine(header, node):
+    node.prev = header.prev
+    node.next = header
+    header.prev.next = node
+    header.prev = node
+
+def searchHeader(n):
+    global Header
+    node = Header.next
+    while node is not Header:
+        if node.num == n: return node
+        node = node.next
+    newNode = Dnode(n)
+    insertLine(Header, newNode)
+    return newNode
+
+def insertCol(col, node):
+    header = searchHeader(col)
+    header.size += 1
+    node.head = header
+    node.down = header
+    node.up   = header.up
+    header.up.down = node
+    header.up = node
+
+def makeDancingLinks(xss):
+    initHeader()
+    for line, xs in enumerate(xss):
+        hNode = Dnode(line)
+        insertCol(xs[0], hNode)
+        for i in xrange(1, len(xs)):
+            node = Dnode(line)
+            insertCol(xs[i], node)
+            insertLine(hNode, node)
+
+def selectMinCol():
+    global Header
+    minNode = Header.next
+    node = minNode.next
+    while node is not Header:
+        if node.size == 0: return node
+        if node.size < minNode.size:
+            minNode = node
+        node = node.next
+    return minNode
+
+def isEmpty():
+    global Header
+    return Header is Header.next
+
+def removeMatrix(hNode):
+    node = hNode
+    while True:
+        header = node.head
+        header.prev.next = header.next
+        header.next.prev = header.prev
+        cNode = node.down
+        while cNode is not node:
+            if cNode is not cNode.head:
+                lNode = cNode.next
+                while lNode is not cNode:
+                    lNode.up.down = lNode.down
+                    lNode.down.up = lNode.up
+                    lNode.head.size -= 1
+                    lNode = lNode.next
+            cNode = cNode.down
+        node = node.next
+        if node is hNode: break
+
+def restoreMatrix(hNode):
+    node = hNode.prev
+    while True:
+        header = node.head
+        header.prev.next = header
+        header.next.prev = header
+        cNode = node.up
+        while cNode is not node:
+            if cNode is not cNode.head:
+                lNode = cNode.prev
+                while lNode is not cNode:
+                    lNode.up.down = lNode
+                    lNode.down.up = lNode
+                    lNode.head.size += 1
+                    lNode = lNode.prev
+            cNode = cNode.up
+        if node is hNode: break
+        node = node.prev
+
+def algoDLX(f, xss):
+    def algoSub(a):
+        if isEmpty():
+            f(a)
+        else:
+            cNode = selectMinCol()
+            lNode = cNode.down
+            while lNode is not cNode:
+                removeMatrix(lNode)
+                a.append(xss[lNode.num])
+                algoSub(a)
+                a.pop()
+                restoreMatrix(lNode)
+                lNode = lNode.down
+    makeDancingLinks(xss)
+    algoSub([])
